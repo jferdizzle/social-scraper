7a832dc9d01e1ec0a4f79a376184496848f0f01f
Julian Ferdman <julianferdman@gmail.com>
2021-05-24 11:31:57 -0400

    filenames should be PG

--- twitter.py
+++ twitter.py
@@ -7,7 +7,7 @@ def scrape_twitter(search_term, since):
     c.Search = search_term
     c.Since = since
     c.Store_csv = True
-    c.Output = "twitter_scrapes/cumrocket_tweets_since_%s.csv" % since
+    c.Output = "twitter_scrapes/tweets_since_%s.csv" % since
     c.Hide_output = True
     c.Stats = True
 
